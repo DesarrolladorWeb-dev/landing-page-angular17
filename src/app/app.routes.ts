6f881b5360b0bc80be9bf7e67188c639b5651304
fernandowilsoncondorponce <condorfernando88@gmail.com>
2024-04-25 13:26:52 -0500

    hicimos una landing page en angular 17

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+// el path la url de cada uno  y su  funcion que son las pages que creamos  se ejecutara al llegar a esta  URL 
+export const routes: Routes = [
+    {path: '', component : HomeComponent},
+    {path: 'products', component : ProductsComponent},
+    {path: 'products/:id', component : ProductDetailComponent},
+    {path: 'contact', component : ContactComponent},
+    // Si pones cualquier te redirige a nada (redirectTo : '' ) 
+    {path: '**', redirectTo:'',pathMatch: 'full'},
+    
+];
